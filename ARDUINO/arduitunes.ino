a593ce399e6e238fd89e1f00b8330067d977b22b
Maximiliano Castro <macguionbajo@gmail.com>
2017-12-28 00:36:06 -0300

    More translations, much clearer variable and function names, and comments

--- ARDUINO/arduitunes.ino
+++ ARDUINO/arduitunes.ino
@@ -17,7 +17,7 @@
 word tempo=200;
 
 char noteStringToFrequency(String s);
-void update();
+void doTick();
 
 
 /* Melody declaration
@@ -178,10 +178,10 @@ const char voice5_melody[] PROGMEM={
 const char* const all_melodies[] PROGMEM={voice1_melody,voice2_melody,voice3_melody,voice4_melody,voice5_melody};
 
 // Active voices
-#define ACTIVE_VOICES 3
+#define ACTIVE_VOICES_COUNT 3
 
 // Voices pins
-char voices_output_pins[ACTIVE_VOICES];
+char voices_output_pins[ACTIVE_VOICES_COUNT];
 
 // Status led pins
 char rhythm_led_pin = 11;
@@ -201,7 +201,8 @@ const uint16_t NOTE_FREQUENCIES[] PROGMEM={31,/*C1*/33,35,37,39,41,44,46,49,52,5
                                3951,/*C8*/4186,4435,4699,4978,0};  //90
 
 
-int delaytempo=7500/(constrain((int)(tempo),30,400));
+// Time period for a 32th-note, derived from the tempo
+int tempo_32th_tick_period=7500/(constrain((int)(tempo),30,400));
 // 7500 comes from (60*1000/tempo)/8. The 8 is derived by considering a demisemiquaver as the fundamental time unit (1/8 of a quarter note)
 
 // Used to parse each voice's note sequence
@@ -220,37 +221,57 @@ int current_notestring_indexes[]={0,0,0,0,0};
 // Pointers for repetitions
 int repetition_indexes[]={-1, -1, -1, -1, -1};
 
-// Each voice note's period (ms) (period = 1/frequency)
-int NOTAS_P[ACTIVE_VOICES];
+// Each voice note's period (microseconds, us) (period = 1/frequency)
+// 'Half period' is the time we must take to toggle the voice's output in order to do it in the desired frequency.
+int current_note_half_periods_us[ACTIVE_VOICES_COUNT];
+
 // Maximum common divider. Voice notes periods must be a multiple of this
 // * Less: More precise notes, higher processing frequency, risks timer callback not finishing at time
-// * More: Less precise notes, no risks for timer callback
+// * More: Less precise notes (untuned), no risks for timer callback
 const char mcd=20; // Jeremy Blum proposes: 64.
 
-// Periods initializer
-void initNotas_p(){
-  for (char i=0;i<ACTIVE_VOICES;i++){
+/**
+ * Initializes time periods for each voice for their current notes.
+ *
+ * - For each active note:
+ *    + Get period from note frequency
+ *    + Adjust period to be a multiple of mcd
+ *    + Update period in global list
+ */
+void computeNotePeriods(){
+  for (char i = 0; i < ACTIVE_VOICES_COUNT; i++){
+    // get current note frequency
     int progint2int=pgm_read_word_near(NOTE_FREQUENCIES + noteStringToFrequency(current_note_strings[i]));
-    int periodo=(int)(500000.0/progint2int);  // Actual note period
-    NOTAS_P[i]=periodo + ((periodo%mcd)<=(mcd/2) ? -(periodo%mcd) : (mcd-(periodo%mcd))); // mcd-adjusted period
+    // derive period from frequency.
+    /* Don't exactly remember where the 500000 comes from. I think that from the fact that:
+     * 1/f[Hz] = P [s]
+     *         = 1000P [ms]
+     *         = 1000000P [us]
+     *  half_period = P/2
+     *  half_period[us] = 500000P
+    */
+    int half_period = (int)(500000.0/progint2int);  // Real half period of this tone
+    current_note_half_periods_us[i] = half_period + ((half_period%mcd)<=(mcd/2) ? -(half_period%mcd) : (mcd-(half_period%mcd))); // mcd-adjusted period
   }
 }
-// Voices tick counters (mutable)
-// using chars instead of ints break silences, don't know why
-unsigned int count[ACTIVE_VOICES];
+// Voices tick counters (mutable). Tracks each voice's current note ticks. Needed to know when to change the note.
+// Using chars instead of ints break silences, don't know why yet
+unsigned int current_note_tick_counts[ACTIVE_VOICES_COUNT];
 
 // Counts per note (max is 252, so unsigned char is enough)
-// 'counts' are the total period divided by the time the timer counter takes to reset.
-// As we want the timer to take 'mcd' us (microseconds), 'counts' is period/mcd.
-// These values are constant throughout execution
-unsigned char counts[ACTIVE_VOICES];
+// 'current_note_tick_limits' symbolize the total period divided by the time the timer counter takes to reset.
+// As we want the timer to take 'mcd' us (microseconds), 'current_note_tick_limits' is period/mcd.
+// If a voice reaches this value, it's current note life ended and we must load the next note.
+unsigned char current_note_tick_limits[ACTIVE_VOICES_COUNT];
 
 
-// 'counts' initializer
-void initCounts(){
-  for (char i=0;i<ACTIVE_VOICES;i++){
-    counts[i]=NOTAS_P[i]/mcd;
-    count[i]=0;
+/**
+ * Initializes the tick counters for each note, used to track each note duration.
+ */
+void initDurationTickCounters(){
+  for (char i=0;i<ACTIVE_VOICES_COUNT;i++){
+    current_note_tick_limits[i] = current_note_half_periods_us[i]/mcd;
+    current_note_tick_counts[i] = 0;
   }
 }
 /**
@@ -260,7 +281,6 @@ void initCounts(){
  * s has two possible shapes:
  *  1. XY
  *  2. XSY
- *
  *
  * @param s: input string
  * @return: int: index to notes list
@@ -296,6 +316,8 @@ char noteStringToFrequency(String s){
       case 71: //G
         pos=7;
         break;
+      default:  // should never happen
+        pos=9;
     }
   }
   else{
@@ -303,20 +325,22 @@ char noteStringToFrequency(String s){
       case 65: //A#
         pos=10;
         break;
-      // B# doesn't exist
+      // B# doesn't exist in equal temperament
       case 67: //C#
         pos=1;
         break;
       case 68: //D#
         pos=3;
         break;
-      // E# doesn't exist
+      // E# doesn't exist in equal temperament
       case 70: //F#
         pos=6;
         break;
       case 71: //G#
         pos=8;
         break;
+      default:  // should never happen
+        pos=10;
     }
   }
   // Magic equation that returns the exact note index
@@ -337,14 +361,14 @@ void initTimer1(){
   TCCR1B |= ((1<<CS11) | (1<<CS10));
   // Set value for the timer counter to match.
   // Timer 1 counter overflows on 65535. Each click takes 4us.
-  // We want to take 'delaytempo' ms.
-  // So, we want to count until X such as 0.004ms * X = delaytempo --> X = delaytempo / 0.004 == delaytempo*250
-  OCR1A=delaytempo*250;
+  // We want to take 'tempo_32th_tick_period' ms.
+  // So, we want to current_note_tick_counts until X such as 0.004ms * X = tempo_32th_tick_period --> X = tempo_32th_tick_period / 0.004 == tempo_32th_tick_period*250
+  OCR1A = tempo_32th_tick_period*250;
 }
 
 void initTimer2(){
   // Disable counter overflow interrupts
-  TIMSK2  &= ~(1<<TOIE2);
+  TIMSK2 &= ~(1<<TOIE2);
   // Enable Compare Match A interrupts
   TIMSK2 |= (1<<OCIE2A);
   // Clear TCCR2A and TCCR2B registers
@@ -356,26 +380,26 @@ void initTimer2(){
   TCCR2B |= (1<<CS21);
   // Set value to match for interrupt triggering
   // Timer 2 counter overflows at 255. We want to reach mcd*2-1
-  OCR2A=mcd*2-1;
+  OCR2A = mcd*2-1;
 }
 
 // Timer 1 interrupt handler (ISR: Interrupt Service Routine)
 ISR(TIMER1_COMPA_vect){
   // tempo led
   PORTB = (tempo_led_counter==7 ? PORTB | _BV(tempo_led_pin -8) : PORTB & ~(_BV(tempo_led_pin-8)));
-  tempo_led_counter=((tempo_led_counter+1)%8);
+  tempo_led_counter = ((tempo_led_counter+1)%8);
   // update notes
-  update();
+  doTick();
 }
 
 // Timer 2 interrupt handler
 ISR(TIMER2_COMPA_vect){
-  // For each voice, increase count and check if the output has to be toggled
-  for (char i=0; i<ACTIVE_VOICES; i++){
-    count[i]++;
-    if (count[i] == counts[i]){
+  // For each voice, increase current_note_tick_counts and check if the output has to be toggled
+  for (char i=0; i<ACTIVE_VOICES_COUNT; i++){
+    current_note_tick_counts[i]++;
+    if (current_note_tick_counts[i] == current_note_tick_limits[i]){
       PORTD ^= (_BV(voices_output_pins[i]));  // much faster than digitalWrite
-      count[i]=0;
+      current_note_tick_counts[i]=0;
     }
   }
 }
@@ -384,68 +408,69 @@ ISR(TIMER2_COMPA_vect){
  * Get next note for this voice and put it in the buffer
  * Here we check for repetitions too
  *
- * @param voz : Voice to read
+ * @param voice_index : Voice for which to read it's next note string from buffer
  */
-void getNext(char voz){
-  if  (current_notestring_indexes[voz]==-1){
+void loadNextVoiceNotestringToBuffer(char voice_index){
+  // if this voice is already finished
+  if (current_notestring_indexes[voice_index] == -1){
     progmem_buffer[0]='X';
     progmem_buffer[1]='\0';
   }
   else{
     // Get pointer to voice string on progmem
-    PGM_P prog_str=(const char*)pgm_read_word(&(all_melodies[voz]));
-    char j=0;
-    boolean fin=false;
-    boolean endrep=false,beginrep=false;
+    PGM_P prog_str = (const char*)pgm_read_word(&(all_melodies[voice_index]));
+    char j = 0;
+    boolean is_voice_end = false;
+    boolean is_repetition_start = false, is_repetition_end = false;
 
     // Read strings until space or 'X'
-    while (((progmem_buffer[j]=(char) pgm_read_byte(&prog_str[current_notestring_indexes[voz]+j]))!= ' ')){
-      if (progmem_buffer[j]=='|'){ // Repetition found
+    while (((progmem_buffer[j]=(char) pgm_read_byte(&prog_str[current_notestring_indexes[voice_index]+j]))!= ' ')){
+      if (progmem_buffer[j]=='|'){ // Repetition start found
         // Move header on next voice
-        current_notestring_indexes[voz]+=4; // Jump 4 characters, as repetition symbol has 4: '||: '
+        current_notestring_indexes[voice_index] += 4; // Jump 4 characters, as repetition symbol has 4: '||: '
         // Store repetition to come back later
-        repetition_indexes[voz]=current_notestring_indexes[voz];
-        beginrep=true;
+        repetition_indexes[voice_index] = current_notestring_indexes[voice_index];
+        is_repetition_start = true;
         // Read next note
         break;
       }
       if (progmem_buffer[j]==':'){ // Repetition end
-        if (repetition_indexes[voz]!=-1){ // If a repetition was started earlier
-          current_notestring_indexes[voz]=repetition_indexes[voz];
-          repetition_indexes[voz]=-1;
+        if (repetition_indexes[voice_index] != -1){ // If a repetition was started earlier
+          current_notestring_indexes[voice_index] = repetition_indexes[voice_index];
+          repetition_indexes[voice_index] =- 1;
         }
         else{  //  Already repeated. Ignore repetition end
-          current_notestring_indexes[voz]+=4;
+          current_notestring_indexes[voice_index] += 4;
         }
-        endrep=true;
+        is_repetition_end = true;
         break;
       }
-      if (progmem_buffer[j]=='X'){
-        fin=true;
+      if (progmem_buffer[j] == 'X'){
+        is_voice_end = true;
         break;
       }
       j++;
     }
-    if (fin){
-      current_notestring_indexes[voz]=-1;
-      progmem_buffer[1]='\0';
+    if (is_voice_end){
+      current_notestring_indexes[voice_index] =- 1;
+      progmem_buffer[1] = '\0';
     }
     else
-    if (beginrep||endrep){  // repetition symbol. Read next note
-        getNext(voz);
+    if (is_repetition_start||is_repetition_end){  // repetition symbol. Read next note
+      loadNextVoiceNotestringToBuffer(voice_index);
       }
     else{
-        current_notestring_indexes[voz]+=(j+1);
-        progmem_buffer[j]='\0';
+        current_notestring_indexes[voice_index] += (j+1);
+        progmem_buffer[j] = '\0';
     }
   }
 }
-boolean melodyOver(){
+boolean isMelodyOver(){
   char c=0;
-  for (int i=0;i<ACTIVE_VOICES;i++){
-    c+=current_notestring_indexes[i];
+  for (int i = 0; i < ACTIVE_VOICES_COUNT; i++){
+    c += current_notestring_indexes[i];
   }
-  return (c==(ACTIVE_VOICES*-1))? true : false;
+  return (c == (ACTIVE_VOICES_COUNT*-1))? true : false;
 }
 
 void endProgram(){
@@ -458,15 +483,16 @@ void endProgram(){
 }
 
 /**
- * Sets the note on the buffer, for the given voice.
+ * For the given voice, update it's state variables according to it's current note string.
  * Notice the note comes in a AB(S)C format that has to be splitted in A and B(S)C. A can have 1 or 2 digits
- * @param voz
+ * @param voice_index: Index to the voice to update
+ *
  */
-void seteaNota(char voz){
+void updateVoiceStateFromBuffer(char voice_index){
   if (progmem_buffer[0]=='X'){  // If no notes left, put silences forever until every voice ends.
-    current_note_strings[voz]="S";
-    current_note_durations[voz]=32;
-    if (melodyOver()){
+    current_note_strings[voice_index]="S";
+    current_note_durations[voice_index]=32;
+    if (isMelodyOver()){
       endProgram();
     }
   }
@@ -474,76 +500,84 @@ void seteaNota(char voz){
     // Set duration. Notice that: 32-> 1 time, 16-> 2 times, 8->4, 4->8, 2->16, 1->32
     String numbers="26";  //  cases 16 and 32
     int d;
-    boolean dosdig=false; //  if duration had 2 digits
+    boolean has_two_digits = false; //  if duration had 2 digits
     if (numbers.indexOf(progmem_buffer[1])!=-1){
-      d=32/((progmem_buffer[0]-'0')*10 + (progmem_buffer[1]-'0'));
-      dosdig=true;
+      d = 32/((progmem_buffer[0]-'0')*10 + (progmem_buffer[1]-'0'));
+      has_two_digits = true;
     }
     else
-      d=32/(progmem_buffer[0]-'0');
-    current_note_durations[voz]=d;
+      d = 32/(progmem_buffer[0]-'0');
+    current_note_durations[voice_index] = d;
 
     // store note
-    char j=dosdig ? 2 : 1; // stands in the note position
-    current_note_strings[voz]=progmem_buffer;
-    current_note_strings[voz]=current_note_strings[voz].substring(j);
+    char j = has_two_digits ? 2 : 1; // stands in the note position
+    current_note_strings[voice_index]=progmem_buffer;
+    current_note_strings[voice_index]=current_note_strings[voice_index].substring(j);
   }
 }
-boolean loadVoices(){
-  // Read each voice duration. If counter is 0, read next note, else, do nothing
-  boolean changed=false;
-  for (char i=0; i<ACTIVE_VOICES; i++){
+
+/**
+ * Update each voice's duration state. If it's duration expired, load next note.
+ *
+ * @return if any voice changed it's note
+ *  true if one or more voices changed their current notes.
+ *  false otherwise
+ */
+boolean updateVoices(){
+  boolean changes_occurred = false;
+  for (char i=0; i<ACTIVE_VOICES_COUNT; i++){
     if (current_note_durations[i]<=1){
-      changed=true;
+      changes_occurred=true;
       // Puts the corresponding note on the progmem_buffer
-      getNext(i);
-      seteaNota(i);
+      loadNextVoiceNotestringToBuffer(i);
+      updateVoiceStateFromBuffer(i);
     }
     else current_note_durations[i]--;
   }
-  return changed;
+  return changes_occurred;
 }
 
-void update(){
-  if (loadVoices()){
-    initNotas_p();
-    initCounts();
+/**
+ * Perform a duration tick
+ *
+ * Each tick may involve updating the current notes being played
+ */
+void doTick(){
+  if (updateVoices()){
+      computeNotePeriods();
+    initDurationTickCounters();
   }
-}
-
-void initBuzzers(){
-  for (char i=0; i<ACTIVE_VOICES; i++){
-    voices_output_pins[i]=i+2;
-    pinMode(i+2,OUTPUT);
-  }
-}
-
-void initLeds(){
-  pinMode(rhythm_led_pin,OUTPUT);
-  pinMode(tempo_led_pin,OUTPUT);
-  digitalWrite(tempo_led_pin,HIGH);
 }
 
 void setup(){
   // Set voice pins
-  initBuzzers();
+  for (char i=0; i<ACTIVE_VOICES_COUNT; i++){
+    voices_output_pins[i] = i+2;
+    pinMode(i+2, OUTPUT);
+  }
   // set indicator led pins
-  initLeds();
+  pinMode(rhythm_led_pin,OUTPUT);
+  pinMode(tempo_led_pin,OUTPUT);
+  digitalWrite(tempo_led_pin,HIGH);
+  // set tempo pot input pin
+  pinMode(0,INPUT);
+
   // Load first note for each voice. Calculate mcd-adjusted note period for each note
-  update();
+  doTick();
+
   // Setup timers
   cli();
   initTimer1();
   initTimer2();
   sei();
-
-  pinMode(0,INPUT);
 }
 
+/** loop() is almost free for anything else.
+ * Notice that Timer0 is still free, so Timer0 functions should kind of work, if the ISR's are quick enough
+ */
 void loop(){
-  // Loop is almost free for anything else. Notice that Timer0 is still free, so Timer0 functions should kind of work
-  tempo=map(analogRead(0),0,1023,30,500);
-  delaytempo=7500/(int)(tempo);
-  OCR1A=delaytempo*250;
+  tempo = map(analogRead(0), 0, 1023, 30, 500);
+  tempo_32th_tick_period = 7500/(int)(tempo);
+  OCR1A = tempo_32th_tick_period*250;
   delay(250);
 }
